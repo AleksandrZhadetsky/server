a9229ebf47e6ed084c7a02a58e40023b14032de3
Aleksandr Zhadetsky <coolweak2@gmail.com>
2024-01-05 20:45:35 +0400

    added api

---
+++ demoapiTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace demoapi.Controllers.Tests
+{
+    [TestClass()]
+    public class WeatherForecastControllerTests
+    {
+
+        [TestMethod()]
+        public void FakeTest()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+
+        [TestMethod()]
+        public void FakeTest1()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+
+        [TestMethod()]
+        public void FakeTest2()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+
+        [TestMethod()]
+        public void FakeTest3()
+        {
+            Assert.AreEqual(2 + 2, 4);
+        }
+    }
+}
